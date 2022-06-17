9c6e92ec34164fe7618e7a0d0da473e35b1467db
OriusMJC <matiasjesuscontreras12@hotmail.com>
2022-06-17 11:43:22 -0300

    Update models

--- api/src/models/Products.ts
+++ api/src/models/Products.ts
@@ -13,25 +13,46 @@ module.exports = (sequelize:any) => {
     },
     title: {
       type: DataTypes.STRING(50),
-      allownull: false
+      allownull: false,
+      validate: {
+        len: [10,50],
+        isAlphanumeric: true,
+        notNull: true,
+      }
     },
     photo: {
       type: DataTypes.STRING,
       allownull: false
     },
     price: {
-      type: DataTypes.INTEGER,
-      allownull: false
+      type: DataTypes.FLOAT,
+      allownull: false,
+      validate: {
+        isNumeric: true,
+        isFloat: true,
+        isAlpha: false,
+        not: /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/,
+        notNull: true,
+        min: 0.01,
+        max: 999999.00,
+      }
     },
     type: {
       type: DataTypes.ENUM('full', 'motherboar', 'procesador', 'grafica', 'ram', 'ssd', 'hdd', 'cooler', 'monitor', 'mouse', 'teclado', 'cables', 'fuente')
     },
     description: {
       type: DataTypes.TEXT,
-      allownull: false
+      allownull: false,
+      validate: {
+        len:[10,500],
+        notNull: true
+      }
     },
     likes: {
       type: DataTypes.INTEGER,
+      validate: {
+        isInt: true,
+      }
     },
     comments: {
       type: DataTypes.ARRAY(DataTypes.JSON),
@@ -42,7 +63,7 @@ module.exports = (sequelize:any) => {
     },
     sell: {
       type: DataTypes.BOOLEAN,
-      default: false
+      defaultValue: false
     },
     sellerInfo: {
       type: DataTypes.JSON,
@@ -51,24 +72,3 @@ module.exports = (sequelize:any) => {
     timestamps: false
   });
 };
-
-// {
-//   "id": "1",
-//   "title" : "Procesador Ryzen 5",
-//   "photo": "url",
-//   "price": 200,
-//   "type": "full | motherboard | procesador | grafica | ram | ssd | hdd | cooler | monitor | mouse | teclado | cables | fuente",
-//   "description": "descripcion Ryzen 5",
-//   "likes": 5,
-//   "comments": [
-//       {"name":"matias", "avatar": "https://i.pravatar.cc/150?u=matias","comment":"Te sirven 50?"}, 
-//       {"name":"Carlitos", "avatar": "https://i.pravatar.cc/150?u=carlos","comment":"De que generacion es??"}
-//       ],
-//   "status": "Nuevo",
-//   "sellerInfo": [{
-//       "id": "1gv2g3v32g2hhd",
-//       "name" : "Marcelo",
-//       "email": "email@gmail.com",
-//       "avatar": "https://i.pravatar.cc/150?u=marcelo"
-//   }]
-// }
