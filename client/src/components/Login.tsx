6a9df910c2197e1d7be850aa53dcca47107232d5
Nicostudent <70242834+Nicostudent@users.noreply.github.com>
2022-06-23 12:17:09 -0300

    login y Register validations

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -5,11 +5,35 @@ import { loginUser } from 'src/redux/actions';
 import { useAppDispatch } from '../config/config'
 import s from './Styles/Login.module.css'
 
-export default function Register(){
+
+interface User {
+  email: string
+  password: string
+}
+
+function validate(user){
+  let errors: User = {
+    email: "",
+    password: ""
+  } 
+  if(!user.email  ){
+      errors.email = '*email required'
+  }else if( !user.password ){
+      errors.password = "*password required"      
+  }  
+  return errors;
+}
+
+export default function Login(){
 
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const [user, setUser] = useState({
+  const [userValidate, setuserValidate] = useState("")
+  const [errors, setErrors] = useState<User>({
+    email: "",
+    password: "",
+  })
+  const [user, setUser] = useState<User>({
     email: "",
     password: "",
   })
@@ -19,31 +43,54 @@ export default function Register(){
       ...user,
       [e.target.name]: e.target.value,
     })
+    setErrors(validate({
+      ...user,
+      [e.target.name] : e.target.value
+  }))
   }
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>){
     e.preventDefault()
-    await userLogin(user.email, user.password)
-    const id = await userData();
-    dispatch(loginUser(id));
-    navigate("/");
+    try {
+      await userLogin(user.email, user.password)
+      const id = await userData();   
+        dispatch(loginUser(id));
+        navigate("/");              
+    } catch (error) {
+      if(error.code === "auth/user-not-found") setuserValidate("User Invalid")   
+       if(error.code === "auth/wrong-password") setuserValidate("Password invalid")   
+      // setuserValidate(error.code)  
+    }
   }
+ 
 
   return (
     <div className={s.formLoginContainer}>
+      {userValidate && <p>{userValidate}</p>}
       <form onSubmit={handleSubmit} className={s.formLogin}>
         <label>Email</label>
-        <input type= "email" placeholder = "Enter email" name="email" value = {user.email} onChange={handleChange}/>
+        <input type= "email"  required minLength={7} placeholder = "example@example.com" name="email" value = {user.email} onChange={handleChange}/>
+        
 
         <label>Password</label>
-        <input type="password" placeholder = "Enter password" name ="password" value = {user.password} onChange={handleChange}/>
-
+        <input type="password" required minLength={6} maxLength={12} placeholder = "Enter password" name ="password" value = {user.password} onChange={handleChange}/>
+        {/* Regex para validar password.  */}
+        {/* pattern='^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$' 
+          ^.*              : Start
+          (?=.{6,12})      : Length min=6 max=12
+          (?=.*[a-zA-Z])   : Letters
+          (?=.*\d)         : Digits
+          (?=.*[!#$%&? "]) : Special characters
+          .*$              : End
+        */}
         <button type="submit">Login</button>
+          <span>Don't have account?</span>
         <Link to='/user/register'>
         <button>Registrarse</button>
       </Link>
       </form>
-     
+      {errors.email && (<p className={s.error}> {errors.email}</p>)}
+      {errors.password && (<p className={s.error}> {errors.password}</p>)}
     </div>
   )
 }
