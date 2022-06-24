25d59757c8a5345ebb8ff8bd1cf60fca9414d979
angelvega0816 <angelvegaxdpro@hotmail.com>
2022-06-24 00:14:30 -0300

    cambios en estilos, modificacion en la slide y componente de loading en home

--- client/src/components/ProductPage/Pages.tsx
+++ client/src/components/ProductPage/Pages.tsx
@@ -1,66 +1,75 @@
-import {useState} from 'react'
-import ProductCard from '../reusable/ProductCard';
-import s from '../Styles/Pages.module.css'
+import { useState } from "react";
+import ProductCard from "../reusable/ProductCard";
+import s from "../Styles/Pages.module.css";
+import './style.css'
 
+function Pages({ productsPerPage, allComponents, refresh }) {
+  const cantPages = Math.ceil(allComponents.length / productsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProduct = allComponents?.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
 
-function Pages({productsPerPage, allComponents, refresh}) {
-     
-    const cantPages = Math.ceil(allComponents.length /productsPerPage) 
-    const [currentPage, setCurrentPage] = useState(1);
-    const indexOfLastProduct = currentPage * productsPerPage;
-    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-    const currentProduct = allComponents?.slice(indexOfFirstProduct, indexOfLastProduct);
+  const pageNumbers = [];
 
-    const pageNumbers = [];
-    
-    if(typeof allComponents !== 'string'){
-        for (let i = 1; i <= cantPages; i++){
-            pageNumbers.push(
-                <button value={i} onClick={()=>{setCurrentPage(i)}}>
-                    {i}
-                </button>)
-        }
+  if (typeof allComponents !== "string") {
+    for (let i = 1; i <= cantPages; i++) {
+      pageNumbers.push(
+        <button
+          value={i}
+          onClick={() => {
+            setCurrentPage(i);
+          }}
+        >
+          {i}
+        </button>
+      );
     }
-    if(currentPage !== 1 && currentPage > cantPages) {
-        setCurrentPage(1)
-    }
+  }
+  if (currentPage !== 1 && currentPage > cantPages) {
+    setCurrentPage(1);
+  }
 
-    return(
-        <section className={s.pageContainer}>
-            <div className={s.buttonsPage}>
-                {pageNumbers}
-            </div>
-            <div className={s.containerProdCards}>
-            {
-                refresh && currentProduct.length?
-                typeof allComponents !== 'string'?
-                currentProduct.map(prod=>{
-                    return(
-                        
-                    <ProductCard 
-                        key={prod && prod.id} 
-                        id={prod && prod.id} 
-                        title={prod && prod.title} 
-                        photo={prod && prod.photo}
-                        price={prod && prod.price} 
-                        type = {prod.type}
-                        likes = {prod.likes}
-                        status = {prod.status}
-
-                        />)
-                })
-                :
-                <h2>{allComponents}</h2>
-                :
-                // <Loading/>
-                <h1>Cargando...</h1>
-            }      
-            </div>
-        </section>
-        
-    )
-
+  return (
+    <section className={s.pageContainer}>
+      <div className={s.buttonsPage}>{pageNumbers}</div>
+      <div className={s.containerProdCards}>
+        {refresh && currentProduct.length ? (
+          typeof allComponents !== "string" ? (
+            currentProduct.map((prod) => {
+              return (
+                <ProductCard
+                  key={prod && prod.id}
+                  id={prod && prod.id}
+                  title={prod && prod.title}
+                  photo={prod && prod.photo}
+                  price={prod && prod.price}
+                  type={prod.type}
+                  likes={prod.likes}
+                  status={prod.status}
+                />
+              );
+            })
+          ) : (
+            <h2>{allComponents}</h2>
+          )
+        ) : (
+          <div className={"loading"}>
+            <div className="ball"></div>
+            <div className="ball"></div>
+            <div className="ball"></div>
+            <div className="ball"></div>
+            <div className="ball"></div>
+            <div className="ball"></div>
+            <div className="ball"></div>
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default Pages;
-
