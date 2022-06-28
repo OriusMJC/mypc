177d417754deb47a9f5d6774c8a3cf49e875f064
OriusMJC <matiasjesuscontreras12@hotmail.com>
2022-06-28 18:48:16 -0300

    commit

---
+++ client/src/components/Loading/Loading.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import s from '../Styles/Loading.module.css'
+import logo from '../../media/logo1.png'
+
+export default function Loading({load,msgError,time}){
+    const [msg,setMsg] = useState(load)
+
+    const temp = setInterval(()=>{
+        setMsg(msgError)
+        clearInterval(temp)
+    },time)
+
+    return(
+        <div className={s.loadContainer}>
+            {
+                msg === load?
+                <>
+                    <img src={logo} alt='logo'/>
+                    <h1>{load}</h1>
+                </>
+                :
+                <>
+                    <h1>Ups</h1>
+                    <h2>{msgError}</h2>
+                </>
+            }
+        </div>
+    )
+}
