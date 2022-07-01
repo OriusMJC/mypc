32faceb4a99f65dd9d8ddca118395a1aa3ad5798
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-06-30 21:02:28 -0300

    validacion rutas put

--- api/src/routes/products.ts
+++ api/src/routes/products.ts
@@ -93,26 +93,65 @@ router.delete('/:idProduct', async(req, res, next) => {
 })
 
 //Modificar la informacion (body) de un producto (params)
-router.put('/:idProduct',async(req,res,next)=>{
+router.put('/:idProduct', [
+    body('photo', 'Ingrese una imagen')
+        .exists(),
+    body('title', 'Ingrese un titulo')
+        .exists()
+        .isLength({max:50}),
+    body('price', 'Ingrese un valor')
+        .exists()
+        .isNumeric(),
+    body('type', 'Ingrese un tipo') 
+        .exists(),
+    body('status', 'Ingrese un estado')
+        .exists(),       
+    body('cant', 'Ingrese una cantidad')
+        .exists()
+        .isNumeric()
+        .isInt(),
+    body('description', 'Ingrese una descripcion del producto')
+        .exists()
+        .isLength({
+                    min: 50,
+                    max: 500
+        })
+],async (req: express.Request, res: express.Response, next:any)=>{//en la documentacion de express-validator res y req estan asi
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(404).json({ errors: errors.array() })
+    } else {
     const newDataProduct = req.body
     const id = req.params.idProduct
     try {
         const response = await updateDataProduct(id, newDataProduct)
         res.json(response)
     } catch (error) {
-        next(error)
+        return next(error)
     }
+}})
+
+router.put('/comments/:idProduct', [
+    body('comment', 'Ingrese un comentario')
+        .exists()
+        .isLength({
+            min: 10,
+            max: 500
 })
-router.put('/comments/:idProduct',async(req,res,next)=>{
+    ],async (req: express.Request, res: express.Response, next:any)=>{//en la documentacion de express-validator res y req estan asi
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(404).json({ errors: errors.array() })
+        } else {
     const id = req.params.idProduct
     const newComment = req.body
     try {
         const response = await addComment(id,newComment)
         res.json(response)
     } catch (error) {
-        next(error)
+        return next(error)
     }
-})
+}})
 
 router.delete('/comments/delete/:idProduct/:idComment', async(req, res, next)=>{
     const {idProduct, idComment} = req.params
@@ -124,16 +163,27 @@ router.delete('/comments/delete/:idProduct/:idComment', async(req, res, next)=>{
     }
 })
 
-router.put('/comments/update/:idProduct', async(req, res, next)=>{
+router.put('/comments/update/:idProduct', [
+    body('response', 'Ingrese un comentario')
+        .exists()
+        .isLength({
+            min: 10,
+            max: 500
+})
+    ],async (req: express.Request, res: express.Response, next:any)=>{//en la documentacion de express-validator res y req estan asi
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(404).json({ errors: errors.array() })
+        } else {
     const id = req.params.idProduct
     const resp = req.body
     try {
         const response = await addSellerResp(id, resp)
         res.json(response)
     } catch (error) {
-        next(error)
+        return next(error)
     }
-})
+}})
 
 router.put('/selled/:idProduct',async(req,res,next)=>{
     const {idProduct} = req.params
