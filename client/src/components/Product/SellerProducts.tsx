6762789a4c643a2582b1fe8bcb43134d130059d7
lautaro2021 <lautaroarnay@gmail.com>
2022-07-04 01:02:52 -0300

    asd

--- client/src/components/Product/SellerProducts.tsx
+++ client/src/components/Product/SellerProducts.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useSelector} from 'react-redux';
 import { useAppDispatch } from "src/config/config"
 import { getAllComponents} from "src/redux/actions"
