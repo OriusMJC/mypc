fc580e1dce90c729314ad573e28eec90755cf8f3
Nicostudent <70242834+Nicostudent@users.noreply.github.com>
2022-07-12 11:10:50 -0300

    about component

--- client/src/components/AboutUs.tsx
+++ client/src/components/AboutUs.tsx
@@ -4,16 +4,20 @@ import angel from '../media/AboutUsImg/angel.png'
 import lauti from '../media/AboutUsImg/lauti.png'
 import mati from '../media/AboutUsImg/mati.png'
 import nico from '../media/AboutUsImg/nico.png'
+import Nico from '../media/AboutUsImg/Nico.jpeg'
 import santi from '../media/AboutUsImg/santi.png'
 import tobias from '../media/AboutUsImg/tobias.png'
-
+import { Link } from 'react-router-dom';
 
 
 interface usData {    
     name: string
-    date: number
-    image: string
+    age: number
+    imageBack: string
+    imageFront: string
     description: string
+    linkedIn: string
+    gitHub: string
 }
 
 
@@ -21,39 +25,57 @@ interface usData {
 const aboutUsDate: Array<usData> =[
     {
         name: "Lauti",
-        date:  1980,
-        image: lauti,   
-        description : "aca tu description" 
+        age:  1980,
+        imageBack: lauti,
+        imageFront: "",   
+        description : "aca tu description" ,
+        linkedIn: "mi linkedin",
+        gitHub: "mi git"
     },
     {
         name: "Mati",
-        date:  1980,
-        image:  mati,   
-        description : "aca tu description"
+        age:  1980,
+        imageBack: mati,
+        imageFront: "",   
+        description : "aca tu description",
+        linkedIn: "mi linkedin",
+        gitHub: "mi git"
     },
     {
         name: "Nico",
-        date:  1980,
-        image: nico,   
-        description : "aca tu description"
+        age:  35,
+        imageBack: nico,
+        imageFront: Nico,   
+        description : "Me gusta comer milanesas",
+        linkedIn: "https://www.linkedin.com/in/nicolasbrojo/",
+        gitHub: "https://github.com/Nicostudent"
     },
     {
         name: "Tobias",
-        date:  1980,
-        image: tobias,   
-        description : "aca tu description"
+        age:  1980,
+        imageBack: tobias,
+        imageFront: "",    
+        description : "aca tu description",
+        linkedIn: "mi linkedin",
+        gitHub: "mi git"
     },
     {
         name: "Angel",
-        date:  1980,
-        image: angel,   
-        description : "aca tu description"
+        age:  1980,
+        imageBack: angel,
+        imageFront: "",   
+        description : "aca tu description",
+        linkedIn: "mi linkedin",
+        gitHub: "mi git"
     },
     {
         name: "Santi",
-        date:  1980,
-        image: santi,   
-        description : "aca tu description"
+        age:  1980,
+        imageBack: santi,
+        imageFront: "",   
+        description : "aca tu description",
+        linkedIn: "mi linkedin",
+        gitHub: "mi git"
     }
 ] 
 
@@ -62,25 +84,34 @@ const aboutUsDate: Array<usData> =[
 function AboutUs() {
     return (
         <div className={s.container}>
-            <h1> About us</h1>
+             <h1>About us</h1>
         <div className={s.gridcontainer}>
+           
             {
                 aboutUsDate.map(user => {
                     return(
-                        <div className={s.eachOfUs}>
-                            <div className={s.name}>
-                                <p>{user.name}</p>
-                                <p>{user.date}</p>
+                        <div className={s.card}>
+                            <div className={`${s.face} ${s.front}`}>
+                                <h3>{user.name}</h3>
+                                <img src={user.imageFront} alt="foto caripela"/>
                             </div>
-                                <p>{user.description}</p>
+                            <div className={`${s.face} ${s.back}`}>                                
+                                  <p>{user.name}</p>
+                                  <p>{user.age}</p>
+                                  <p>{user.description}</p>
+                                  <div className={s.redes}>
+                                  <Link  to={user.linkedIn}><i className='fa fa-linkedin-square' ></i></Link>                                  
+                                  <Link to={user.gitHub}><i className='fa fa-github-square'></i></Link>                                
+                                  </div>
+                              <img src={user.imageBack} alt={user.name}/>
+                            </div>
 
-                            <img src={user.image} alt={user.name}/>
                         </div>
                     )
                 })
             }
             </div>
-        </div>
+         </div>
     )
 }
 
